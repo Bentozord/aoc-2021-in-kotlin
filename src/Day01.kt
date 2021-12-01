869d07ba4196942cfd3635c9f8e6413e6ef62e3a
Bentozord <rafal.utnicki26@gmail.com>
2021-12-01 17:55:22 +0100

    aoc day 1, both parts

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,31 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Long {
+        return input.zipWithNext()
+                .stream()
+                .filter { pair -> pair.first < pair.second }
+                .count()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+
+    fun part2(input: List<Int>): Long {
+        val result = mutableListOf<Int>()
+        for (i in 0..input.size - 3) {
+            result.add(sumThreeElements(input, i))
+        }
+        return part1(result)
     }
+
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("Day01_test").map { it.toInt() }
+    check(part1(testInput) == 7L)
+    check(part2(testInput) == 5L)
 
-    val input = readInput("Day01")
+    val input = readInput("Day01").map { it.toInt() }
     println(part1(input))
     println(part2(input))
+}
+
+fun sumThreeElements(input: List<Int>, i: Int): Int {
+    return input[i] + input[i + 1] + input[i + 2]
 }
